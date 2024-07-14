73737b43bbb84902c8a5e7adc2dd365d81a6006b
Tovisi Zoltan <tovisi.zoltan@student.ms.sapientia.ro>
2024-07-14 13:48:54 +0300

    FINAL VERSION

--- music-app-frontend/src/app/app.component.ts
+++ music-app-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterModule, RouterOutlet} from '@angular/router';
 import {ArtistListComponent} from "./components/artist-list/artist-list.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, ArtistListComponent],
+    imports: [RouterOutlet, ArtistListComponent, RouterModule, CommonModule  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
